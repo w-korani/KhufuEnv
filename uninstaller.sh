cca46fe6205297e5cbd5cb9854ae3a12ef278c44
Walid Korani <wkorani@hudsonalpha.org>
2025-03-21 15:02:01 -0500

    Update uninstaller.sh

--- uninstaller.sh
+++ uninstaller.sh
@@ -2,7 +2,9 @@
 
 CDIR="$(pwd)"
 
-if test -d /etc/KhufuEnv; then rm -rf /etc/KhufuEnv; fi
-sed -i "/^source \/etc\/KhufuEnv\/call.sh$/d"  ~/.bashrc
-
+if test -d /etc/KhufuEnv; then 
+rm -rf /etc/KhufuEnv
 echo "Successfully uninstalled; please reload ~/.bashrc"
+else
+echo "KhufuEnv is already uninstalled"
+fi
