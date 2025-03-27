645395d1dc1556258cda2dea42efe342e431a7f9
Walid Korani <wkorani@hudsonalpha.org>
2025-03-27 06:11:22 -0500

    Update call.sh

--- src/call.sh
+++ src/call.sh
@@ -1,10 +1,10 @@
-dir="/etc/KhufuEnv"
-source ""$dir"/01.KhufuEnvHapmap.sh"
-source ""$dir"/02.KhufuEnvPanmap.sh"
-source ""$dir"/03.KhufuEnvStats.sh"
-source ""$dir"/04.KhufuEnvFasta.sh"
-source ""$dir"/05.KhufuEnvFastq.sh"
-source ""$dir"/06.KhufuEnvDataSet.sh"
-PATH=$PATH:/"$dir"
+khufu_dir="/etc/KhufuEnv"
+source ""$khufu_dir"/01.KhufuEnvHapmap.sh"
+source ""$khufu_dir"/02.KhufuEnvPanmap.sh"
+source ""$khufu_dir"/03.KhufuEnvStats.sh"
+source ""$khufu_dir"/04.KhufuEnvFasta.sh"
+source ""$khufu_dir"/05.KhufuEnvFastq.sh"
+source ""$khufu_dir"/06.KhufuEnvDataSet.sh"
+PATH=$PATH:/"$khufu_dir"
 export PATH
-KhufuEnvHelp(){ fun=$1; if [ -z $fun ]; then echo "";  echo "KhufuEnv.Ver1.0.0";  cat "$dir"/[0-9][0-9].KhufuEnv*.txt | awk 'BEGIN{X=1}{if($0 ~ "^[A-Za-z]+" ){printf "%03d: %s\n", X,$0; X+=1}else{print $0} }'  ; echo "" ; else cat "$dir"/[0-9][0-9].KhufuEnv*.doc | awk -v fun=$fun 'BEGIN{x=0;print "\n#####################\nKhufuEnv.Ver1.0.0: "fun} {if(x==1 && $0!="##END_"fun"##" ){print $0}; if($0=="###BEGIN_"fun"###"){x=1}; if($0=="##END_"fun"##"){x=0} }END{print "#####################\n"}'  | tr '\t' ' '; fi; }
+KhufuEnvHelp(){ fun=$1; if [ -z $fun ]; then echo "";  echo "KhufuEnv.Ver1.0.0";  cat "$khufu_dir"/[0-9][0-9].KhufuEnv*.txt | awk 'BEGIN{X=1}{if($0 ~ "^[A-Za-z]+" ){printf "%03d: %s\n", X,$0; X+=1}else{print $0} }'  ; echo "" ; else cat "$khufu_dir"/[0-9][0-9].KhufuEnv*.doc | awk -v fun=$fun 'BEGIN{x=0;print "\n#####################\nKhufuEnv.Ver1.0.0: "fun} {if(x==1 && $0!="##END_"fun"##" ){print $0}; if($0=="###BEGIN_"fun"###"){x=1}; if($0=="##END_"fun"##"){x=0} }END{print "#####################\n"}'  | tr '\t' ' '; fi; }
