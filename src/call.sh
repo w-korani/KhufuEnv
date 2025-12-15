862da12aca8711c02638b60e94adf821ab8f9704
ktdavis0 <114546375+ktdavis0@users.noreply.github.com>
2025-12-15 14:36:40 -0600

    Update call.sh

--- src/call.sh
+++ src/call.sh
@@ -5,7 +5,7 @@ source ""$khufu_dir"/03.KhufuEnvStats.sh"
 source ""$khufu_dir"/04.KhufuEnvFasta.sh"
 source ""$khufu_dir"/05.KhufuEnvFastq.sh"
 source ""$khufu_dir"/06.KhufuEnvDataSet.sh"
-PATH=$PATH:/"$khufu_dir"
+PATH=$PATH:"$khufu_dir"
 export PATH
 KhufuEnvHelp(){ fun=$1; if [ -z $fun ]; then echo "";  echo "KhufuEnv.Ver1.0.0";  cat "$khufu_dir"/[0-9][0-9].KhufuEnv*.txt | awk 'BEGIN{X=1}{if($0 ~ "^[A-Za-z]+" ){printf "%03d: %s\n", X,$0; X+=1}else{print $0} }'  ; echo "" ; 
 else if ! grep -q "###BEGIN_${fun}###" "$khufu_dir"/[0-9][0-9].KhufuEnv*.doc; then
