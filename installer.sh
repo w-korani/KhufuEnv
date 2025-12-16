fadc1cd9e646e092ba44cdbb0c9b0b429829530c
ktdavis0 <114546375+ktdavis0@users.noreply.github.com>
2025-12-15 18:22:01 -0600

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -16,3 +16,27 @@ chmod 555 ~/bin/KhufuEnv/*.sh
 g++ ~/bin/KhufuEnv/utilities/phred_score_stat.cpp -o ~/bin/KhufuEnv/utilities/phred_score_stat
 
 echo "Successfully installed"
+
+check_R_pkg() {
+    pkg="$1"
+    Rscript -e "if (nzchar(system.file(package='$pkg'))) quit(status=0) else quit(status=1)" \
+        > /dev/null 2>&1
+}
+
+if command -v Rscript >/dev/null 2>&1 ; then 
+    echo "Checking required R packages..."
+
+    req_pkgs="data.table ggplot2 Rcpp"
+    for i in $req_pkgs; do 
+        if check_R_pkg "$i"; then
+            echo "$i is already installed."
+        else 
+            echo "$i package is missing."
+            echo "Installing $i..."
+            Rscript -e "install.packages('$i')"
+        fi
+    done
+
+else
+    echo "Warning: Rscript not found. Please install R. Skipping R package checks."
+fi
